1d00d2d4ac36cd2fb1de2bc2190491dadaf60e16
Влад Летун <fyks1649@gmail.com>
2019-06-01 12:47:49 +0300

    Updates

---
+++ precache-manifest.fa629d0bcd164a24af6c945e7236640a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efbf9fea798af22d9ceb5e7d3791cf2",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8daea13147b65dda97fd",
+    "url": "/react-app/static/css/main.848be4cd.chunk.css"
+  },
+  {
+    "revision": "27b0ee05a0de71ffbd9a",
+    "url": "/react-app/static/js/2.2118023b.chunk.js"
+  },
+  {
+    "revision": "8daea13147b65dda97fd",
+    "url": "/react-app/static/js/main.ba4338b1.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
